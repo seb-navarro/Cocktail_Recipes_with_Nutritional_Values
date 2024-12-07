8445ce5a69f180b77eb6e7ee1435263284360898
tom <141278466+tomlukewarden@users.noreply.github.com>
2024-12-07 11:58:55 +0000

    edited card stylings

--- api/cocktail.js
+++ api/cocktail.js
@@ -8,21 +8,23 @@ const CocktailApiKey = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s
             }
 
             fetch(`${CocktailApiKey}${query}`)
-                .then(response => response.json())
-                .then(data => {
-                    const results = data.drinks || [];
-                    const resultsContainer = document.getElementById('results');
-                    resultsContainer.innerHTML = '';
-                    results.forEach(drink => {
-                        const card = document.createElement('div');
-                        card.classList.add('card');
-                        card.classList.add('col-md-4');
-                        card.innerHTML = `
-                            <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}">
-                            <h2>${drink.strDrink}</h2>
-                            <a href="ingredients.html?cocktail=${drink.idDrink}" class="btn btn-dark mb-1" target="_blank">More info</a>
-                        `;
-                        resultsContainer.appendChild(card);
+    .then(response => response.json())
+    .then(data => {
+        const results = data.drinks || [];
+        const resultsContainer = document.getElementById('results');
+        resultsContainer.innerHTML = '';
+        results.forEach(drink => {
+            const card = document.createElement('div');
+            card.classList.add('card', 'col-md-4', 'text-white', 'mb-4', 'p-5', 'g-3');
+            card.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+            card.innerHTML = `
+                <img src="${drink.strDrinkThumb}" class="card-img-top" alt="${drink.strDrink}" style="height: 200px; object-fit: cover;">
+                <div class="card-body">
+                    <h5 class="card-title">${drink.strDrink}</h5>
+                    <a href="ingredients.html?cocktail=${drink.idDrink}" class="btn btn-primary btn-sm" target="_blank">More Info</a>
+                </div>
+            `;
+            resultsContainer.appendChild(card);
                     });
                 })
                 .catch(error => {
